581e63d5665b11e0812ed725bb6777543b4b4739
liquid11 <orangevolution@yahoo.com>
2014-10-16 01:16:45 +0500

    initial template load

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//script.js
+
+//module
+var arsalanApp = angular.module('arsalanApp',['ui-router']);
+
+//controller
+
+arsalanApp.controller('mainController',function($scope){
+
+    $scope.message = "Hello  and welcome to test-run";
+});
+
+
+
